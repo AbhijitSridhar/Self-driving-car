04be0e3f5f8e5bee5874e46d048eb6563334050d
Abhijit Sridhar <32199226+AbhijitSridhar@users.noreply.github.com>
2018-04-07 12:37:35 +0530

    Add files via upload

--- blackdetect.py
+++ blackdetect.py
@@ -17,8 +17,21 @@ while True:
     low_threshold = 50
     high_threshold = 150
     canny_edges = cv2.Canny(gauss_gray,low_threshold,high_threshold)
+#This next part works only if edges are being detected. It turns off immediately if no edges are seen
+#    rho = 2
+#    theta = np.pi/180
+#    threshold = 20
+#    min_line_len = 50
+#    max_line_gap = 200
+#    lines = cv2.HoughLinesP(canny_edges,rho,theta,threshold,np.array([]),minLineLength = 50, maxLineGap = 200)
+#    line_img = np.zeros((canny_edges.shape[0],canny_edges.shape[1],3),dtype = np.uint8)
 
-    cv2.imshow('edges',canny_edges)
+#    for line in lines:
+#        for x1,y1,x2,y2 in line:
+#            cv2.line(line_img,(x1,y1),(x2,y2),[255,0,0],2)
+
+#    cv2.imshow('edges',line_img)
+    cv2.imshow('out',canny_edges)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
